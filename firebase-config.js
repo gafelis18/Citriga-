986c314f696886694298d116b983e09b85041056
gafelis18 <garciafelis@hotmail.com>
2025-05-23 07:52:47 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
+// Configuración Firebase para Citriga
+const firebaseConfig = {
+  apiKey: "AIzaSyB3tPLJRLz6-Ib-EChCm86le3vIqwTITbE",
+  authDomain: "citriga-a636e.firebaseapp.com",
+  projectId: "citriga-a636e",
+  storageBucket: "citriga-a636e.firebasestorage.app",
+  messagingSenderId: "751824862962",
+  appId: "1:751824862962:web:1ee9ecd5e92a5342c61a6b"
+};
 
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
