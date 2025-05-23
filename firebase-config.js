0097251546632a3747aa6cf7648effd114bba389
gafelis18 <garciafelis@hotmail.com>
2025-05-23 07:59:21 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3tPLJRLz6-Ib-EChCm86le3vIqwTITbE",
+  authDomain: "citriga-a636e.firebaseapp.com",
+  projectId: "citriga-a636e",
+  storageBucket: "citriga-a636e.firebasestorage.app",
+  messagingSenderId: "751824862962",
+  appId: "1:751824862962:web:1ee9ecd5e92a5342c61a6b"
+};
+
+firebase.initializeApp(firebaseConfig);
+console.log("Firebase cargado correctamente");
