935be28c2aab3a610c47227fb2c7bf36416a94cb
gafelis18 <garciafelis@hotmail.com>
2025-05-23 07:32:44 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "citriga-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "dashboard.html",
+  "clientes.html",
+  "proveedores.html",
+  "contratos.html",
+  "albaranes.html",
+  "anticipos.html",
+  "factura.html",
+  "style.css",
+  "app.js",
+  "firebase-config.js"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
