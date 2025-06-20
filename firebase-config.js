ddb238192e0399ae2b87b241311999c648e8b970
gafelis18 <garciafelis@hotmail.com>
2025-06-20 16:54:26 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Reemplaza los valores por tu configuración real
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
